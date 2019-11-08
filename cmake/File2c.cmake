35ca897b840fd1fb5f0d7cfbc0d74b74500ecafb
Alex Choi <choiip@gmail.com>
2019-11-08 23:40:54 +0800

    improved build speed

--- cmake/File2c.cmake
+++ cmake/File2c.cmake
@@ -1,24 +1,51 @@
 # portably convert binary file to header
 function(FILE2C INFILE VARNAME OUTFILE)
-  file(READ ${INFILE} HEXFILE HEX)
-  string(LENGTH ${HEXFILE} XRSLEN)
-  set(HEXPOS 0)
+  if(NOT DEFINED INFILE)
+    message(FATAL_ERROR
+      "Required cmake variable INFILE not set!"
+    )
+  endif()
+
+  if(NOT DEFINED OUTFILE)
+    message(FATAL_ERROR
+      "Required cmake variable OUTFILE not set!"
+    )
+  endif()
+
+  if(NOT DEFINED VARNAME)
+    message(FATAL_ERROR
+      "Required cmake variable VARNAME not set!"
+    )
+  endif()
+
+  if(NOT EXISTS ${INFILE})
+    message(FATAL_ERROR "File '${INFILE}' does not exist!")
+  endif()
+
+  file(READ "${INFILE}" contents HEX)
+
   file(WRITE ${OUTFILE}
-          "/* generated from ${INFILE}; do not edit */\n"
-          "const unsigned char ${VARNAME}[] = {")
-  while(${HEXPOS} LESS ${XRSLEN})
-    math(EXPR LPOS "${HEXPOS} % 32")
-    if(NOT ${LPOS})
-      file(APPEND ${OUTFILE} "\n")
-    endif(NOT ${LPOS})
-    string(SUBSTRING ${HEXFILE} ${HEXPOS} 2 HEXBYTE)
-    file(APPEND ${OUTFILE} "0x${HEXBYTE}")
-    math(EXPR HEXPOS "${HEXPOS} + 2")
-    if(${HEXPOS} LESS ${XRSLEN})
-      file(APPEND ${OUTFILE} ",")
-    endif(${HEXPOS} LESS ${XRSLEN})
-  endwhile(${HEXPOS} LESS ${XRSLEN})
-  file(APPEND ${OUTFILE} "};\n")
+          "/* Generated from ${INFILE}; do not edit */\n"
+          "#pragma once\n")
+
+  file(APPEND "${OUTFILE}" "#ifdef __cplusplus\n")
+  file(APPEND "${OUTFILE}" "extern \"C\" {\n")
+  file(APPEND "${OUTFILE}" "#endif\n")
+
+  file(APPEND "${OUTFILE}" "const unsigned char "
+  "${VARNAME}[] = {\n")
+
+  string(REGEX MATCHALL ".." output "${contents}")
+  string(REGEX REPLACE ";" ",\n  0x" output "${output}")
+
+  file(APPEND "${OUTFILE}" "  0x${output}\n")
+
+  file(APPEND "${OUTFILE}" "};\n")
+
+  file(APPEND "${OUTFILE}" "#ifdef __cplusplus\n")
+  file(APPEND "${OUTFILE}" "}\n")
+  file(APPEND "${OUTFILE}" "#endif\n")
+
 endfunction(FILE2C)
 
 FILE2C(${INFILE} ${VARNAME} ${OUTFILE})
